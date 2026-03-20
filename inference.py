fix: opencc t2s.json suffix bug, fix uint8 overflow in rgb_ints

inference.py
@@ -29,7 +29,7 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False 
 
-cc = opencc.OpenCC('t2s.json')
+cc = opencc.OpenCC('t2s')
 set_seed(SEED)
 converter_t2s = opencc.OpenCC('t2s')
 